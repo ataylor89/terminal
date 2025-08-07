0cc2b63d39c49c93b0e4a7e0e460a3dd5ab0683b
Andrew Taylor <rolyata47@gmail.com>
2025-08-07 18:54:34 -0500

    Adding gui

--- terminal.py
+++ terminal.py
@@ -1,45 +1,103 @@
-import threading
+# Please note, this is still a work in progress
+# It's not done... there is still a lot of work to do
+
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
 import subprocess
+import threading
 import time
+import fcntl
+import os
 import logging
 
 logger = logging.getLogger(__name__)
 
-def readuserinput(process):
-    userinput = ""
-    while userinput != "exit":
-        userinput = input("% ")
-        process.stdin.write(userinput)
-        process.stdin.write("\n")
-        process.stdin.flush()
-        time.sleep(0.1)
-    process.terminate()
-    logger.info("Exiting the readuserinput thread...")
+class GUI(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title("Terminal")
+        self.geometry("900x600")
+        self.prefix = "% "
+        self.frame = tk.Frame(self)
+        self.frame.pack(fill="both", expand=True)
+        self.text_area = ScrolledText(self.frame, wrap="word")
+        self.text_area.pack(fill="both", expand=True)
+        self.text_area.bind("<Return>", self.handle_enter)
+        self.append_prefix()
+        self.protocol("WM_DELETE_WINDOW", self.handle_close)
 
-def readprocessoutput(process):
-    while True:
-        line = process.stdout.readline()
-        if not line:
-            break
-        print(line, end="")
-    logger.info("Exiting the readprocessoutput thread...")
+    def set_shell(self, shell):
+        self.shell = shell
 
-def main():
-    logging.basicConfig(filename="terminal.log", level=logging.INFO)
-    logger.info("Started terminal.py")
-    try:
-        process = subprocess.Popen(
-            ["/bin/bash", "-i"], 
-            stdin = subprocess.PIPE,
-            stdout = subprocess.PIPE,
-            stderr = subprocess.PIPE,
-            text=True)
-        t1 = threading.Thread(target=readuserinput, args=(process,))
-        t2 = threading.Thread(target=readprocessoutput, args=(process,))
-        t1.start()
-        t2.start()
-    except Exception as err:
-        logger.error(err)
+    def append(self, text):
+        self.text_area.insert(tk.END, text)
+        self.text_area.see(tk.END)
+
+    def append_prefix(self):
+        self.text_area.insert(tk.END, self.prefix)
+        self.text_area.see(tk.END)
+
+    def handle_enter(self, event):
+        line = self.text_area.index('end-1c').split('.')[0]
+        userinput = self.text_area.get(f"{line}.2", f"{line}.end")
+        logger.info("User input = %s" %userinput)
+        self.shell.write(userinput)
+
+    def handle_close(self):
+        self.shell.stop_event.set()
+        self.destroy()
+
+class Shell:
+    def __init__(self, gui, prefix="% "):
+        self.prefix = prefix
+        self.gui = gui
+        try:
+            self.process = subprocess.Popen(
+                ["/bin/bash", "-i"],
+                stdin = subprocess.PIPE,
+                stdout = subprocess.PIPE,
+                stderr = subprocess.PIPE,
+                text=True)
+            fcntl.fcntl(self.process.stdout.fileno(), fcntl.F_SETFL, os.O_NONBLOCK)
+            logger.info("Started bash process")
+            self.thread = threading.Thread(target=self.readloop)
+            self.stop_event = threading.Event()
+            self.thread.start()
+            logger.info("Started thread")
+        except Exception as err:
+            logger.error(err)
+
+    def set_prefix(self, prefix):
+        self.prefix = prefix
+
+    def readloop(self):
+        logger.info("Starting readloop thread...")
+        while not self.stop_event.is_set():
+            buffer = ""
+            done = False
+            while not done:
+                try:
+                    buffer += self.process.stdout.read()
+                except:
+                    done = True
+            if buffer:
+                self.gui.append(buffer)
+                self.gui.append_prefix()
+        logger.info("Stopping readloop thread...")
+
+    def write(self, userinput):
+        self.process.stdin.write(userinput)
+        self.process.stdin.write("\n")
+        self.process.stdin.flush()
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(
+        filename="terminal.log",
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+    logger.info("Starting up...")
+    gui = GUI()
+    shell = Shell(gui)
+    gui.set_shell(shell)
+    gui.mainloop()
