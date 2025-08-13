2fcba56f9e2f7f2b8bd1a1a041139da5e8306924
Andrew Taylor <rolyata47@gmail.com>
2025-08-13 14:17:52 -0500

    Adding util.search utility method

--- util.py
+++ util.py
@@ -4,3 +4,6 @@ def split(text, delimiter=" "):
     pattern = r'{0}(?=(?:[^"\']*["\'][^"\']*["\'])*[^"\']*$)'.format(re.escape(delimiter))
     return [token.strip() for token in re.split(pattern, text)]
 
+def search(text, substr):
+    pattern = r'{0}(?=(?:[^"\']*["\'][^"\']*["\'])*[^"\']*$)'.format(re.escape(substr))
+    return re.search(pattern, text)
