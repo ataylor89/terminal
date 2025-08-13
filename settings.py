dabce6537252b52c34292bd7d98f8a46c1c59928
Andrew Taylor <rolyata47@gmail.com>
2025-08-12 20:15:00 -0500

    Simplifying settings

--- settings.py
+++ settings.py
@@ -8,5 +8,3 @@ class Settings:
         self.font = ("SF Mono Regular", 16)
         self.prefix = "% "
         self.homedir = os.path.expanduser("~")
-        self.wp_path = self.homedir + "/Github/WordProcessor/target/WordProcessor.jar"
-        self.paint_path = self.homedir + "/Github/Paint/target/Paint.jar"
