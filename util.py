ee227fc38a961b77bc5678d244a7b2a84a990627
Andrew Taylor <rolyata47@gmail.com>
2025-08-13 14:20:25 -0500

    Renaming parameter from substr to delimiter

--- util.py
+++ util.py
@@ -4,6 +4,6 @@ def split(text, delimiter=" "):
     pattern = r'{0}(?=(?:[^"\']*["\'][^"\']*["\'])*[^"\']*$)'.format(re.escape(delimiter))
     return [token.strip() for token in re.split(pattern, text)]
 
-def search(text, substr):
-    pattern = r'{0}(?=(?:[^"\']*["\'][^"\']*["\'])*[^"\']*$)'.format(re.escape(substr))
+def search(text, delimiter):
+    pattern = r'{0}(?=(?:[^"\']*["\'][^"\']*["\'])*[^"\']*$)'.format(re.escape(delimiter))
     return re.search(pattern, text)
