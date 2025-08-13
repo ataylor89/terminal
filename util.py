6a845ed1b9ffed8ea666f13660ba1e673771b14b
Andrew Taylor <rolyata47@gmail.com>
2025-08-13 11:59:10 -0500

    Making the function easier to use

--- util.py
+++ util.py
@@ -1,9 +1,9 @@
 import shlex
 
-def split(text, delimiter=None):
+def split(text, delimiter=None, remove_quotes=True):
     lexer = shlex.shlex(text, posix=True)
     lexer.whitespace_split = True
-    lexer.quotes = ""
     if delimiter:
         lexer.whitespace = delimiter
+    lexer.quotes = "'\"" if remove_quotes else ""
     return [item.strip() for item in list(lexer)]
