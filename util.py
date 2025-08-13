d9bab3753049953e126a12c02b2f2c5e15638aa5
Andrew Taylor <rolyata47@gmail.com>
2025-08-13 11:51:18 -0500

    Disabling quote removal

--- util.py
+++ util.py
@@ -3,8 +3,7 @@ import shlex
 def split(text, delimiter=None):
     lexer = shlex.shlex(text, posix=True)
     lexer.whitespace_split = True
-    lexer.commenters = ""
+    lexer.quotes = ""
     if delimiter:
-        lexer.whitespace += delimiter
-        lexer.wordchars = lexer.wordchars.replace(delimiter, "")
-    return list(lexer)
+        lexer.whitespace = delimiter
+    return [item.strip() for item in list(lexer)]
