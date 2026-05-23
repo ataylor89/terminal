3eb0fee25be326ce77414a895343cb5a4883338f
Andrew Taylor <rolyata47@gmail.com>
2026-05-23 18:01:02 -0500

    Simplifying code, using pty

--- terminal.py
+++ terminal.py
@@ -1,14 +1,41 @@
-import gui
-import shell
-import parser
+import tkinter as tk
+import os
+import pty
+import threading
 
-def main():
-    config = parser.parse_config()
-    sh = shell.Shell(config)
-    Gui = gui.GUI(config)
-    sh.set_gui(Gui)
-    Gui.set_shell(sh)
-    Gui.mainloop()
+class Terminal(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.title('Terminal')
+        width = self.winfo_screenwidth()
+        height = self.winfo_screenheight()
+        self.geometry(f'{width}x{height}+0+0')
+        self.text = tk.Text(self, background='blue', foreground='white')
+        self.text.pack(expand=True, fill='both')
+        self.pid, self.parent_fd = pty.fork()
+        if self.pid == 0:
+            os.execlp('bash', 'bash')
+        else:
+            self.text.bind('<Key>', self.write_to_pty)
+            threading.Thread(target=self.read_from_pty, daemon=True).start()
 
-if __name__ == "__main__":
-    main()
+    def read_from_pty(self):
+        while True:
+            try:
+                data = os.read(self.parent_fd, 1024).decode(errors='ignore')
+                if data:
+                    self.text.insert('end', data)
+                    self.text.see('end')
+            except OSError:
+                break
+
+    def write_to_pty(self, event):
+        char = event.char
+        if event.keysym == 'Return':
+            char = '\n'
+        os.write(self.parent_fd, char.encode())
+        return 'break'
+
+if __name__ == '__main__':
+    gui = Terminal()
+    gui.mainloop()
